b1a1301cf7dc044fc777812172fe421f33f85c37
Zstzbxn <zstzbxn@pop-os.localdomain>
2023-05-20 14:35:56 +0200

    fix syntax in a <p>

--- frontend/app/components/Welcome.tsx
+++ frontend/app/components/Welcome.tsx
@@ -7,7 +7,7 @@ const Welcome: React.FC = () => {
     <div className="flex flex-col items-center justify-center h-full gap-3">
       <h1 className="text-4xl font-bold">Welcome to Certification dapp</h1>
       <p className="text-xl">Please use your <Link className='underline font-semibold' href={"/certification"}>Certification ID</Link> or your <Link className='underline font-semibold' href={"/certificationList"}>student ID</Link> to see your certifications.</p>
-      <p className="text-xl">You chan check the students IDs list <Link className='underline font-semibold' href={"/studentsList"}>here</Link> and use them to retrieve certifications IDs</p>
+      <p className="text-xl">You chan check the students IDs list <Link className='underline font-semibold' href={"/studentsList"}>here</Link> and use them to retrieve certifications IDs.</p>
       <p className="text-xl">Admins can use this <Link className='underline font-semibold' href={"/admin"}>page</Link> to create new certifications.</p>
     </div>
   )
